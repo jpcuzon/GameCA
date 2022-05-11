16c6fd5c7be76c201ee5f1c6b7f2220cb5cd9c8f
Leo Macieski <leojkmacieski@Hotmail.com>
2022-05-11 20:11:17 +0100

    map+village
    map + village

--- src/gameca/AssetManager.java
+++ src/gameca/AssetManager.java
@@ -36,9 +36,29 @@ public class AssetManager {
         gp.object[2].worldX = 8 * gp.tileSize;
         gp.object[2].worldY = 14 * gp.tileSize;
         
-        gp.object[3] = new ObjVillageHouse();
+        gp.object[3] = new ObjVillageHouse(1);
         gp.object[3].worldX = 13 * gp.tileSize;
-        gp.object[3].worldY = 41 * gp.tileSize;
+        gp.object[3].worldY = 40 * gp.tileSize;
+        
+        gp.object[4] = new ObjVillageHouse(1);
+        gp.object[4].worldX = 20 * gp.tileSize;
+        gp.object[4].worldY = 45 * gp.tileSize;
+        
+        gp.object[5] = new ObjVillageHouse(1);
+        gp.object[5].worldX = 17 * gp.tileSize;
+        gp.object[5].worldY = 37 * gp.tileSize;
+        
+        gp.object[6] = new ObjVillageHouse(2);
+        gp.object[6].worldX = 20 * gp.tileSize;
+        gp.object[6].worldY = 41 * gp.tileSize;
+        
+        gp.object[7] = new ObjVillageHouse(2);
+        gp.object[7].worldX = 12 * gp.tileSize;
+        gp.object[7].worldY = 36 * gp.tileSize;
+        
+        gp.object[8] = new ObjVillageHouse(2);
+        gp.object[8].worldX = 15 * gp.tileSize;
+        gp.object[8].worldY = 44 * gp.tileSize;
     }
     
 }
