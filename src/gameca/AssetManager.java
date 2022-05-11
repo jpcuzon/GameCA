8c40cdedd4552161831ab7d520fea4aa9fa27eac
Leo Macieski <leojkmacieski@Hotmail.com>
2022-05-11 18:33:23 +0100

    colision
    colision

--- src/gameca/AssetManager.java
+++ src/gameca/AssetManager.java
@@ -29,16 +29,16 @@ public class AssetManager {
         gp.object[0].worldY = 3 * gp.tileSize;
         
         gp.object[1] = new ObjChest();
-        gp.object[1].worldX = 2 * gp.tileSize;
-        gp.object[1].worldY = 15 * gp.tileSize;
+        gp.object[1].worldX = 40 * gp.tileSize;
+        gp.object[1].worldY = 21 * gp.tileSize;
         
         gp.object[2] = new ObjGate();
         gp.object[2].worldX = 8 * gp.tileSize;
         gp.object[2].worldY = 14 * gp.tileSize;
         
         gp.object[3] = new ObjVillageHouse();
-        gp.object[3].worldX = 30 * gp.tileSize;
-        gp.object[3].worldY = 15 * gp.tileSize;
+        gp.object[3].worldX = 13 * gp.tileSize;
+        gp.object[3].worldY = 41 * gp.tileSize;
     }
     
 }
